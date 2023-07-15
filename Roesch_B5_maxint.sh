fd59d7683c4026d87a5182e222199ebf66bf7a71
RoeschFlo <rosch.flo96@gmail.com>
2023-07-15 16:48:09 +0200

    B5 fertig

---
+++ Roesch_B5_maxint.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+#Author: Rösch Florian
+#Name: B.5 maxint in bash
+#Date:07.14.2023 (m.d.y)
+
+for (( j=1000000000000000000; j>0; j/=10 )); do 
+    echo "Zahl die dazu addiert wird $j"
+    for ((i=k ; i>-1; )); do 
+        k=$i
+       i=$(( i + j ))
+       echo "Aktueller Zählerstand $i"
+    done   
+done
+
+echo "Stand nach überlauf $i"
+maxint=$(( i - 1))
+echo "Maximal möglche positive Zahl $maxint"
